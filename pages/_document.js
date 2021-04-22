21820233ba24e16109620b3fb8136acf7535841b
Anthony Pizzurro <anthony@anthmatic.com>
2021-04-22 10:43:22 -0400

    anti flicker

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,22 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `.async-hide { opacity: 0 !important}`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+              h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+              (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+              })(window,document.documentElement,'async-hide','dataLayer',4000,
+              {'OPT-KS6WG8G':true});
+          `,
+            }}
+          />
         </Head>
         <body>
           <noscript
